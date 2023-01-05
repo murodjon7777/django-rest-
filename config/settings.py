593552da4a6c930fc00c1a1c9d41ce379e0ffe5a
murodjon7777 <88221222+murodjon7777@users.noreply.github.com>
2023-01-05 16:24:07 +0500

    host

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=i6_*fq&q^8+1xuia!4uv^p55nbiq2gepmthnibj(=w!w-f8xc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
